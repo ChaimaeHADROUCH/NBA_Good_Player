a5281ff7b8a1dbc8f34ecc63d4f3447c72bc30ac
ChaimaeHADROUCH <96015964+ChaimaeHADROUCH@users.noreply.github.com>
2022-10-11 16:51:15 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import home, result
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('result/', result, name='result')
-]
